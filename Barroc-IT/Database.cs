eef7e3c3a9f067a7f30cd40f069e74b8570ba9a2
Michael Speekenbrink <d172181@edu.rocwb.nl>
2016-10-20 09:09:30 +0200

    Database toegevoegd

---
+++ Barroc-IT/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barroc_IT
+{
+    class Database
+    {
+        private static string connectionString;
+        private static Database instance;
+
+        private static Database()
+        {
+            connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\michael\Documents\Radius College\N4 Applicatieontwikkelaar\Leerjaar 2\Project\Barroc\Bouwfase\Barroc-IT\DatabaseBarrocIT.accdb";
+        }
+
+        public static Database GetInstace()
+        {
+            if (instance == null)
+            {
+                instance = new Database();
+            }
+
+            return instance;
+        }
+    }
+}
