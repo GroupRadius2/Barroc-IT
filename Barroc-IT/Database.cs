01a627fabb9730c0ca918f5e0a1f7d0984a2bb97
LeviVlasblom <D168228@edu.rocwb.nl>
2016-11-02 10:46:27 +0100

    database string

--- Barroc-IT/Database.cs
+++ Barroc-IT/Database.cs
@@ -20,7 +20,7 @@ namespace Barroc_IT
         private Database()
         {
             //connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DBBarroc.mdf;Integrated Security=True;Connect Timeout=30";
-            connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Levi\Documents\Visual Studio 2013\Projects\Barroc-IT\Barroc-IT\App_Data\ReBarroc.mdf;Integrated Security=True;Connect Timeout=30";
+            connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=|DataDirectory|\App_Data\ReBarroc.mdf;Integrated Security=True;Connect Timeout=30";
             adapter = new SqlDataAdapter();
             connection = new SqlConnection(connectionString);
         }
