3c7c5cc36563238eb22444a95b469dfb31b4d86a
D170264 <d170264@edu.rocwb.nl>
2016-11-03 11:32:03 +0100

    Query's gefixt

--- Barroc-IT/Development.cs
+++ Barroc-IT/Development.cs
@@ -28,9 +28,9 @@ namespace Barroc_IT
             this.dataGridView4.DefaultCellStyle.Font = new Font("Tahoma", 16);
             this.dataGridView4.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             Database.GetInstance().QueryInDatagridView("Select c_name, c_address, c_housenumber,c_code,c_city,c_contactperson_first_name, c_contactperson_last_name, c_contactperson_initials,c_contactperson_telephone_number,c_contactperson_faxnumber, c_contactperson_email, c_potential_customer, c_last_contact_date, c_creditworthy, c_revenue, c_limit, c_ledger, c_btw_code, c_maintenance_contract  FROM tbl_companies ", dataGridView2);
-            Database.GetInstance().QueryInDatagridView("Select p_start_date, p_end_date, p_progression FROM tbl_project", dataGridView3);
-            Database.GetInstance().QueryInDatagridView("Select a_date , a_time_of FROM tbl_appiontments", dataGridView1);
-            Database.GetInstance().QueryInDatagridView("Select p_progression , p_start_date, p_end_date  FROM tbl_project", dataGridView4);
+            Database.GetInstance().QueryInDatagridView("Select p_start_date, p_end_date, p_progression FROM tbl_projects", dataGridView3);
+            Database.GetInstance().QueryInDatagridView("Select a_date , a_time_of FROM tbl_appointments", dataGridView1);
+            Database.GetInstance().QueryInDatagridView("Select p_progression , p_start_date, p_end_date  FROM tbl_projects", dataGridView4);
             dataGridView1.Columns[0].HeaderCell.Value = "Date";
             dataGridView1.Columns[1].HeaderCell.Value = "Time";
             dataGridView3.Columns[0].HeaderCell.Value = "Start date";
