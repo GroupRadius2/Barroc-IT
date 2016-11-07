601035ea15787b196c4152eded03cd4423371da6
LeviVlasblom <D168228@edu.rocwb.nl>
2016-11-07 11:33:23 +0100

    db

--- Barroc-IT/Sales dash.cs
+++ Barroc-IT/Sales dash.cs
@@ -144,22 +144,21 @@ namespace Barroc_IT
 
         private void btnChSave_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
             Database.GetInstance().Query("UPDATE tbl_companies SET c_name = @c_name, c_address = @c_address, c_housenumber = @c_housenumber, c_city = @c_city, c_contactperson_first_name = @c_contactperson_first_name, " +
                 "c_contactperson_telephone_number = @c_contactperson_telephone_number, @c_contactperson_faxnumber = @c_contactperson_faxnumber, c_contactperson_email = @c_contactperson_email, c_contactperson_telephone_number2 = @c_contactperson_telephone_number2, c_city2 = @c_city2, c_zipcode = @c_zipcode, c_zipcode2 = @c_zipcode2, c_address2 = @c_address2");
             Database.GetInstance().AddParameter("@c_name", txtbChCompanyName.Text);
-            Database.GetInstance().AddParameter("@c_address", textBoxAddress1.Text);
-            Database.GetInstance().AddParameter("@c_housenumber", textBoxHousenumber.Text);
-            Database.GetInstance().AddParameter("@c_city", textBoxResidence1.Text);
-            Database.GetInstance().AddParameter("@c_contactperson_first_name", textBoxContactPerson.Text);
-            Database.GetInstance().AddParameter("@c_contactperson_telephone_number", textBoxTelephone1.Text);
-            Database.GetInstance().AddParameter("@c_contactperson_faxnumber", textBoxFaxnumber.Text);
-            Database.GetInstance().AddParameter("@c_contactperson_telephone_number2", textBoxTelephone2.Text);
-            Database.GetInstance().AddParameter("@c_contactperson_email", textBoxEmail.Text);
-            Database.GetInstance().AddParameter("@c_zipcode", textBoxZipcode2.Text);
-            Database.GetInstance().AddParameter("@c_zipcode2", textBoxZipcode2.Text);
-            Database.GetInstance().AddParameter("@c_address2", textBoxAddress2.Text);
-            Database.GetInstance().AddParameter("@c_city2", textBoxResidence2.Text);
+            Database.GetInstance().AddParameter("@c_address", txtbChAdress.Text);
+            Database.GetInstance().AddParameter("@c_housenumber", txtbChHousenumber.Text);
+            Database.GetInstance().AddParameter("@c_city", txtbChCity.Text);
+            Database.GetInstance().AddParameter("@c_contactperson_first_name", txtbChContactperson.Text);
+            Database.GetInstance().AddParameter("@c_contactperson_telephone_number", txtbChTelephone.Text);
+            Database.GetInstance().AddParameter("@c_contactperson_faxnumber", txtbChFaxnumber.Text);
+            Database.GetInstance().AddParameter("@c_contactperson_telephone_number2", txtbChTelephone2.Text);
+            Database.GetInstance().AddParameter("@c_contactperson_email", txtbChEmail.Text);
+            Database.GetInstance().AddParameter("@c_zipcode", txtbChZipcode.Text);
+            Database.GetInstance().AddParameter("@c_zipcode2", txtbChZipcode2.Text);
+            Database.GetInstance().AddParameter("@c_address2", txtbChAdress2.Text);
+            Database.GetInstance().AddParameter("@c_city2", txtbChCity2.Text);
 
             Database.GetInstance().ExecuteQuery();
             UpdateInfo();
